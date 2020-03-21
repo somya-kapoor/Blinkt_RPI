6e8db79f21e1e0ea6cb418aa28d4a2db21cbb02c
somya-kapoor <42409030+somya-kapoor@users.noreply.github.com>
2020-03-21 07:06:05 +0530

    Blinkt_RPI
    Add files via upload
    
    Create static
    
    Delete static
    
    Add files via upload
    
    Rename master.css to static/master.css
    
    Add files via upload
    
    Rename rain.html to templates/rain.html
    
    Add files via upload
    
    Update app.py
    
    Update index.html
    
    Update master.css
    
    Update master.css
    
    Delete rain.html
    
    Update app.py
    
    Update index.html
    
    Update app.py
    
    Update master.css
    
    Update app.py
    
    Update app.py
    
    Update app.py
    
    Update index.html
    
    Update app.py
    
    Update app.py
    
    Update index.html
    
    Update master.css
    
    Update index.html
    
    Update index.html
    
    Update app.py
    
    Update app.py
    
    update
    
    Update app.py
    
    Create README.md
    
    Add files via upload
    
    Update README.md
    
    Update README.md
    
    Update README.md
    
    Update README.md
    
    Update README.md

---
+++ README.md
@@ -0,0 +1,13 @@
+# Blinkt_RPI
+## Controlling Blinkt on Raspberry Pi through a Web Page
+## Folder Tree
+* Blinkt_RPI
+    * app.py
+    * static
+        * master.css
+    * templates
+        * index.html
+## Working
+![](https://github.com/somya-kapoor/Blinkt_RPI/blob/master/Working.png )
+## Web Page
+![](https://github.com/somya-kapoor/Blinkt_RPI/blob/master/Webpage.png )
---
+++ app.py
@@ -0,0 +1,51 @@
+
+from flask import *
+from blinkt import set_pixel, set_brightness, show, clear
+import colorsys
+import time
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():	
+    return render_template('index.html')
+
+@app.route('/off')
+def off():
+    clear()
+    show()
+    return render_template('index.html')
+
+@app.route('/mwl')
+def mwl():    
+    t = 2
+    while t > 0:
+        for i in range(8):
+            clear()
+            set_pixel(i, 255, 255, 255)
+            show()
+            time.sleep(0.05)
+        t = t - 1  
+    return render_template('index.html')  
+        
+@app.route('/rainbow')
+def rainbow():     
+    spacing = 360.0 / 16.0
+    hue = 0
+    set_brightness(0.1)
+    t = 16
+    while t > 0:
+        hue = int(time.time() * 100) % 360
+        for x in range(8):
+            offset = x * spacing
+            h = ((hue + offset) % 360) / 360.0
+            r, g, b = [int(c * 255) for c in colorsys.hsv_to_rgb(h, 1.0, 1.0)]
+            set_pixel(x, r, g, b)
+        show()
+        time.sleep(0.05)
+        t = t - 1
+            
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
